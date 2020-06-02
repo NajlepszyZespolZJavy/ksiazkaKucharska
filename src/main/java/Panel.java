3f93a16c0db6751589146e6ba61732ab9166b72e
przemekbarczyk <przemek.barczyk@gmail.com>
2020-06-02 22:23:19 +0200

    Zmodyfikowano klasę Panel i plik pom.xml
    Do konstruktora klasy Panel dodano instrukcje tworzące komponenty przycisków w lewym dolnym i prawym dolnym rogu panelu.

--- src/main/java/Panel.java
+++ src/main/java/Panel.java
@@ -1,8 +1,10 @@
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JButton;
 import java.awt.Color;
 
 public class Panel extends JPanel {
+
     public static final int WIDTH = 1200;
     public static final int HEIGHT = 800;
 
@@ -15,6 +17,22 @@ public class Panel extends JPanel {
 //        ImageIcon tlo_grafika = new ImageIcon("src\\main\\java\\grafiki\\tlo.jpg");
 //        tlo.setIcon(tlo_grafika);
         setLayout(null);
+
+        // strzałka w lewo
+        JButton strzalkaLewo = new JButton();
+        strzalkaLewo.setBounds(25, 650, 100, 100);
+//        ImageIcon strzalkaLewoGrafika = new ImageIcon("src\\main\\java\\grafiki\\strzalka_lewo.png");
+//        strzalkaLewo.setIcon(strzalkaLewoGrafika);
+        strzalkaLewo.setContentAreaFilled(true);
+        tlo.add(strzalkaLewo);
+
+        // strzałka w prawo
+        JButton strzalkaPrawo = new JButton();
+        strzalkaPrawo.setBounds(1075, 650, 100, 100);
+//        ImageIcon strzalkaPrawoGrafika = new ImageIcon("src\\main\\java\\grafiki\\strzalka_prawo.png");
+//        strzalkaPrawo.setIcon(strzalkaPrawoGrafika);
+        strzalkaPrawo.setContentAreaFilled(true);
+        tlo.add(strzalkaPrawo);
 
         add(tlo);
         setVisible(true);
