7c7e3bed1ff92d32e244ade7b48c3c3a19b48fa9
przemekbarczyk <przemek.barczyk@gmail.com>
2020-06-04 11:55:02 +0200

    Dodano klasy PrzykladowyPrzepis i PrzykladowyPrzepis2
    Są to testowe klasy na podstawie których zostanie stworzony układ Panelu.

---
+++ src/main/java/PrzykladowyPrzepis2.java
@@ -0,0 +1,31 @@
+public class PrzykladowyPrzepis2 {
+
+    String tytul = "Nazwa drugiego przepisu";
+    String sciezkaGrafiki = "src\\main\\java\\grafiki\\przepis2.jpg";
+    String czasWykonania = "Czas wykonania: y godziny";
+    String trudnoscWykonania = "Trudność wykonania: y";
+    String skladniki = "<html>Składniki:<br>" +
+            "250g masła<br>" +
+            "200ml wody<br>" +
+            "8 jajek<br>" +
+            "1l mleka<br>" +
+            "2 żółtka<br>" +
+            "coś jeszcze<br>" +
+            "coś jeszcze<br>" +
+            "coś jeszcze";
+    String instrukcje = "<html>Instrukcje:<br>" +
+            "1) <br>" +
+            "2) <br>" +
+            "3) <br>" +
+            "4) <br>" +
+            "5) <br>" +
+            "6) <br>" +
+            "7)";
+
+    protected String getTytul() { return tytul; }
+    protected String getSciezkaGrafiki() { return sciezkaGrafiki; }
+    protected String getCzasWykonania() { return czasWykonania; }
+    protected String getTrudnoscWykonania() { return trudnoscWykonania; }
+    protected String getSkladniki() { return skladniki; }
+    protected String getInstrukcje() { return instrukcje; }
+}
