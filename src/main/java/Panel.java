24b49318ea8a4d6b8c99d850cf76b378828a242e
Marcin <mbrozek31@gmail.com>
2020-06-04 12:55:49 +0200

    Zmodyfikowano klasę Panel
    Dodano do Panelu komponenty odpowiedzialne za wyświetlanie informacji o przepisie.

--- src/main/java/Panel.java
+++ src/main/java/Panel.java
@@ -1,7 +1,10 @@
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -45,6 +48,55 @@ public class Panel extends JPanel implements ActionListener, KeyListener {
         przyciskPrawo.addActionListener(this);
         przyciskPrawo.addKeyListener(this);
         tlo.add(przyciskPrawo);
+
+        PrzykladowyPrzepis aktualnyPrzepis = new PrzykladowyPrzepis();
+
+        // tytuł przepisu
+        JLabel tytul = new JLabel();
+        tytul.setBounds(0,0,600,50);
+        tytul.setText(aktualnyPrzepis.getTytul());
+        tytul.setFont(new Font(Font.SERIF, Font.BOLD,30));
+        tytul.setHorizontalAlignment(SwingConstants.CENTER);
+        tytul.setText(aktualnyPrzepis.getTytul());
+        tlo.add(tytul);
+
+        // grafika przepisu
+        JLabel grafika = new JLabel();
+        grafika.setBounds(5, 50, 600, 200);
+        grafika.setIcon(new ImageIcon(aktualnyPrzepis.getSciezkaGrafiki()));
+        tlo.add(grafika);
+
+        // czas wykonania
+        JLabel czasWykonania = new JLabel();
+        czasWykonania.setBounds(10,260,600,40);
+        czasWykonania.setFont(new Font(Font.SERIF, Font.PLAIN,20));
+        czasWykonania.setVerticalAlignment(SwingConstants.TOP);
+        czasWykonania.setText(aktualnyPrzepis.getCzasWykonania());
+        tlo.add(czasWykonania);
+
+        // trudność wykonania
+        JLabel trudnoscWykonania = new JLabel();
+        trudnoscWykonania.setBounds(10,300,600,40);
+        trudnoscWykonania.setFont(new Font(Font.SERIF, Font.PLAIN,20));
+        trudnoscWykonania.setVerticalAlignment(SwingConstants.TOP);
+        trudnoscWykonania.setText(aktualnyPrzepis.getTrudnoscWykonania());
+        tlo.add(trudnoscWykonania);
+
+        // składniki
+        JLabel skladniki = new JLabel();
+        skladniki.setBounds(10,350,600,550);
+        skladniki.setFont(new Font(Font.SERIF, Font.PLAIN,20));
+        skladniki.setVerticalAlignment(SwingConstants.TOP);
+        skladniki.setText(aktualnyPrzepis.getSkladniki());
+        tlo.add(skladniki);
+
+        // instrukcje
+        JLabel instrukcje = new JLabel();
+        instrukcje.setBounds(610,10,600,800);
+        instrukcje.setFont(new Font(Font.SERIF, Font.PLAIN,20));
+        instrukcje.setVerticalAlignment(SwingConstants.TOP);
+        instrukcje.setText(aktualnyPrzepis.getInstrukcje());
+        tlo.add(instrukcje);
 
         add(tlo);
         setVisible(true);
