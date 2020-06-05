80e31f964bb70cc03d27af5f9392111c20ae606d
kuba <kuba.bialon2@gmail.com>
2020-06-05 16:10:17 +0200

    Dodano klasę Przepis
    Klasa przepis jest klasą abstrakcyjną z której będą dziedziczyły klasy przedstawiające poszczególne przepisy.

---
+++ src/main/java/Przepis.java
@@ -0,0 +1,16 @@
+public abstract class Przepis {
+
+    protected String tytul;
+    protected String sciezkaGrafiki;
+    protected String czasWykonania;
+    protected String trudnoscWykonania;
+    protected String skladniki;
+    protected String instrukcje;
+
+    protected abstract String getTytul();
+    protected abstract String getSciezkaGrafiki();
+    protected abstract String getCzasWykonania();
+    protected abstract String getTrudnoscWykonania();
+    protected abstract String getSkladniki();
+    protected abstract String getInstrukcje();
+}
