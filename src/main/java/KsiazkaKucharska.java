072e7edd07ac2a0968393ba3156c98f8218c107c
przemekbarczyk <przemek.barczyk@gmail.com>
2020-06-12 23:17:27 +0200

    Dodano dokumentacje do KsiazkaKucharska.java

--- src/main/java/KsiazkaKucharska.java
+++ src/main/java/KsiazkaKucharska.java
@@ -1,4 +1,10 @@
+/**
+ * Główna klasa łącząca w całość pozostałe klasy występujące w programie
+ */
 public class KsiazkaKucharska {
+    /**
+     * Główna metoda od której zaczyna się praca programu.
+     */
     public static void main(String[] args) {
         new Ramka();
     }
