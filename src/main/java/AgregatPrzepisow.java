6eecbe7c6bedb4b8c331dc8e974b2245407f2635
Marcin <mbrozek31@gmail.com>
2020-06-13 11:12:24 +0200

    Dodano dokumentację do klasy AgregatPrzepisow.java

--- src/main/java/AgregatPrzepisow.java
+++ src/main/java/AgregatPrzepisow.java
@@ -1,19 +1,31 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Klasa agregująca wszystkie przepisy znajdujące się w książce kucharskiej
+ */
 public class AgregatPrzepisow {
 
     private List <Przepis> listaPrzepisow = new ArrayList<Przepis>();
 
+    /**
+     * Konstruktor zapełnia listę wszystkimi przepisami
+     */
     public AgregatPrzepisow() {
         listaPrzepisow.add(new PrzykladowyPrzepis());
         listaPrzepisow.add(new PrzykladowyPrzepis2());
     }
 
+    /**
+     * Zwraca listePrzepisow.
+     */
     public List <Przepis> getListaPrzepisow() {
         return listaPrzepisow;
     }
 
+    /**
+     * Tworzy iterator po lisciePrzepisow.
+     */
     public IteratorPrzepisow iterator() {
         return new IteratorPrzepisow(getListaPrzepisow());
     }
